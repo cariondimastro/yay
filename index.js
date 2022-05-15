abedf9dfcbc7c388cd996938ea230df1ae90816f
Mari 98 <>
2022-05-15 13:49:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var { precogas, precoalc } = newFunction()
+var div = parseInt(precogas,10) / (precoalc,10)
+
+
+if (div > 0.7){
+  alert('a gasolina compensará mais.')
+}
+if (div < 0.7){
+  alert('o álcool compensará mais.')
+}
+else if (div = 0.7){
+  alert('não há diferença entre os combustíveis.')
+}
+
+  alert('deseja fazer outro cálculo?') 
+  alert('1 - Sim')
+  alert('2 - Não')
